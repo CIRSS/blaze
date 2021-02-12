b8c968ed214aa14cc51546627c22425a4a86a46a
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-02-11 21:02:04 -0800

    Tune service start scripts

--- service/run.sh
+++ service/run.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# avoid error message on ctrl-c
-cleanup() {
-    echo
-    exit 0
-}
-trap cleanup EXIT
+# # avoid error message on ctrl-c
+# cleanup() {
+#     echo
+#     exit 0
+# }
+# trap cleanup EXIT
 
 # run the service
 echo
 echo "--------------------------------------------------------------------------"
-echo "The Blazegraph service will now start in the REPRO."
+echo "The Blazegraph service is now running in the REPRO."
 echo "Connect to it by navigating in a web browser to http://localhost:9999 "
 echo
-echo "Terminate the service by typing CTRL-C in this terminal."
+echo "Terminate the service by pressing the 'q' key in this terminal."
 echo "--------------------------------------------------------------------------"
-cd ${BLAZEGRAPH_DOT_DIR}
-${BLAZEGRAPH_CMD} 2>&1 > `eval echo ${BLAZEGRAPH_LOG}`
+while [ true ] ; do
+    read -n 1 key
+    if [[ $key = 'q' ]] ; then
+        echo
+        exit ;
+    else
+        echo 
+        echo "Type 'q' key to stop the Blazegraph service."
+    fi
+
+done
