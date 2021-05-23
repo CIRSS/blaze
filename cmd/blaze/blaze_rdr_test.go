597c891bc3e33c17a265eae86237642ba2fe65d3
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-05-22 20:38:04 -0700

    More queries of first RDR example

--- cmd/blaze/blaze_rdr_test.go
+++ cmd/blaze/blaze_rdr_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cirss/go-cli/pkg/util"
 )
 
-func TestBlazegraphCmd_query_rdfstar(t *testing.T) {
+func TestBlazegraphCmd_rdr_query_bobs_age(t *testing.T) {
 
 	var outputBuffer strings.Builder
 	Program.OutWriter = &outputBuffer
@@ -28,24 +28,85 @@ func TestBlazegraphCmd_query_rdfstar(t *testing.T) {
 
 	Program.AssertExitCode(t, "blaze import --format ttlx --dataset rdr", 0)
 
-	query := `
-		PREFIX bigdata: <http://bigdata.com/>
-		PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-		PREFIX dct:  <http://purl.org/dc/elements/1.1/>
-		
-		SELECT ?age ?src WHERE {
-			?bob foaf:name "Bob" .
-			<<?bob foaf:age ?age>> dct:source ?src .
-		}
-	`
+	assert_query_result := func(query, result string) {
+		outputBuffer.Reset()
+		Program.InReader = strings.NewReader(query)
+		Program.AssertExitCode(t, "blaze query --format table --dataset rdr", 0)
+		util.LineContentsEqual(t, outputBuffer.String(), result)
+	}
 
-	outputBuffer.Reset()
-	Program.InReader = strings.NewReader(query)
-	Program.AssertExitCode(t, "blaze query --format table --dataset rdr", 0)
-
-	util.LineContentsEqual(t, outputBuffer.String(),
-		`age | src
-        =============================================
-        23  | http://example.net/homepage-listing.html
+	t.Run("just_bobs_age", func(t *testing.T) {
+		assert_query_result(`
+			PREFIX bigdata: <http://bigdata.com/>
+			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+			PREFIX dct:  <http://purl.org/dc/elements/1.1/>
+			
+			SELECT ?age 
+			WHERE {
+				?bob foaf:name "Bob" .
+				?bob foaf:age ?age.
+			}
+		`,
+			`age
+        	 ==
+        	 23  
 		`)
+	})
+
+	t.Run("bobs_age_and_its_source", func(t *testing.T) {
+		assert_query_result(`
+			PREFIX bigdata: <http://bigdata.com/>
+			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+			PREFIX dct:  <http://purl.org/dc/elements/1.1/>
+			
+			SELECT ?age ?src
+			WHERE {
+				?bob foaf:name "Bob" .
+				<<?bob foaf:age ?age>> dct:source ?src .
+			}
+		`,
+			`age | src
+	         =============================================
+    	     23  | http://example.net/homepage-listing.html
+		`)
+	})
+
+	t.Run("provenance_of_bobs_age", func(t *testing.T) {
+		assert_query_result(`
+			PREFIX bigdata: <http://bigdata.com/>
+			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+			PREFIX dct:  <http://purl.org/dc/elements/1.1/>
+			
+			SELECT ?predicate ?src
+			WHERE {
+				?bob foaf:name "Bob" .
+				<<?bob foaf:age 23>> ?predicate ?src .
+			}
+		`,
+			`predicate                               | src
+			 =================================================================================
+			 http://purl.org/dc/elements/1.1/creator | http://example.com/crawlers#c1
+			 http://purl.org/dc/elements/1.1/source  | http://example.net/homepage-listing.html
+		`)
+	})
+
+	t.Run("everyones_ages_and_their_provenance", func(t *testing.T) {
+		assert_query_result(`
+			PREFIX bigdata: <http://bigdata.com/>
+			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+			PREFIX dct:  <http://purl.org/dc/elements/1.1/>
+
+			SELECT ?name ?age ?predicate ?src 
+			WHERE {
+				?person foaf:name ?name .
+				<<?person foaf:age ?age>> ?predicate ?src .
+			}
+		`,
+			`name | age | predicate                               | src
+			 ==============================================================================================
+			 Bob  | 23  | http://purl.org/dc/elements/1.1/creator | http://example.com/crawlers#c1
+			 Bob  | 23  | http://purl.org/dc/elements/1.1/source  | http://example.net/homepage-listing.html
+		`)
+	})
+
 }
