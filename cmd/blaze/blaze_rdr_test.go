30abc91785fdbc9dab6e8d5d3338f880f5a04edd
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-05-24 15:05:23 -0700

    Demonstrate multiple levels of reification

--- cmd/blaze/blaze_rdr_test.go
+++ cmd/blaze/blaze_rdr_test.go
@@ -116,7 +116,7 @@ func TestBlazegraphCmd_rdr_query_alice_bob_and_charlies_ages(t *testing.T) {
 	Program.OutWriter = &outputBuffer
 	Program.ErrWriter = &outputBuffer
 
-	Program.Invoke("blaze destroy --dataset rdr --quiet")
+	Program.Invoke("blaze destroy --dataset rdr --quiet ")
 	Program.Invoke("blaze create --quiet --dataset rdr --rdfstar")
 
 	Program.InReader = strings.NewReader(`
@@ -226,4 +226,113 @@ func TestBlazegraphCmd_rdr_query_alice_bob_and_charlies_ages(t *testing.T) {
 			 Charlie | 25
 			`)
 	})
+
+	t.Run("everything_sam_is_the_source_of", func(t *testing.T) {
+		assert_query_result(`
+			PREFIX bigdata: <http://bigdata.com/>
+			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+			PREFIX dct:  <http://purl.org/dc/elements/1.1/>
+
+			SELECT ?s ?p ?o 
+			WHERE {
+				?src foaf:name "Sam" .
+				<<?s ?p ?o>> dct:source ?src .
+			}
+			`,
+			`s                          | p                             | o
+			 ================================================================
+			 http://bigdata.com/alice   | http://xmlns.com/foaf/0.1/age | 21
+			 http://bigdata.com/bob     | http://xmlns.com/foaf/0.1/age | 23
+			 http://bigdata.com/charlie | http://xmlns.com/foaf/0.1/age | 25
+			`)
+	})
+
+	t.Run("everything_triple_that_has_a_triple_for_its_subject", func(t *testing.T) {
+		assert_query_result(`
+			PREFIX bigdata: <http://bigdata.com/>
+			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+			PREFIX dct:  <http://purl.org/dc/elements/1.1/>
+
+			SELECT ?s ?p ?o ?pp ?oo
+			WHERE {
+				<<?s ?p ?o>> ?pp ?oo .
+			}
+			`,
+			`s                          | p                             | o  | pp                                     | oo
+			 ====================================================================================================================================
+			 http://bigdata.com/alice   | http://xmlns.com/foaf/0.1/age | 21 | http://purl.org/dc/elements/1.1/source | http://bigdata.com/sam
+			 http://bigdata.com/bob     | http://xmlns.com/foaf/0.1/age | 23 | http://purl.org/dc/elements/1.1/source | http://bigdata.com/sam
+			 http://bigdata.com/charlie | http://xmlns.com/foaf/0.1/age | 25 | http://purl.org/dc/elements/1.1/source | http://bigdata.com/sam
+			 http://bigdata.com/charlie | http://xmlns.com/foaf/0.1/age | 27 | http://purl.org/dc/elements/1.1/source | http://bigdata.com/joe
+			`)
+	})
+}
+
+func TestBlazegraphCmd_rdr_query_two_levels_of_reification(t *testing.T) {
+
+	var outputBuffer strings.Builder
+	Program.OutWriter = &outputBuffer
+	Program.ErrWriter = &outputBuffer
+
+	insert_data := func(data string) {
+		Program.InReader = strings.NewReader(data)
+		Program.AssertExitCode(t, "blaze import --format ttlx --dataset rdr", 0)
+	}
+
+	assert_query_result := func(query, result string) {
+		outputBuffer.Reset()
+		Program.InReader = strings.NewReader(query)
+		Program.AssertExitCode(t, "blaze query --format table --dataset rdr", 0)
+		util.LineContentsEqual(t, outputBuffer.String(), result)
+	}
+
+	Program.Invoke("blaze destroy --dataset rdr --quiet ")
+	Program.Invoke("blaze create --quiet --dataset rdr --rdfstar")
+
+	insert_data(`
+		@prefix x: <http://example/> .
+		<x:a1> <x:b1> <x:c1> .
+	`)
+
+	assert_query_result(`
+		SELECT ?s ?p ?o
+		WHERE {
+			?s ?p ?o .
+		}`,
+		`s    | p    | o
+         ===================
+         x:a1 | x:b1 | x:c1
+		`)
+
+	insert_data(`
+		@prefix x: <http://example/> .
+		<<<x:a> <x:b> <x:c>>> <x:d> <x:e> .
+	`)
+
+	assert_query_result(`
+		SELECT ?s ?p ?o ?pp ?oo
+		WHERE {
+			<<?s ?p ?o>> ?pp ?oo .
+		}
+		`,
+		`s   | p   | o   | pp  | oo
+		 ==============================
+		 x:a | x:b | x:c | x:d | x:e
+		`)
+
+	insert_data(`
+		@prefix x: <http://example/> .
+		<<<<<x:a> <x:b> <x:c>>> <x:d> <x:e>>> <x:f> <x:g> .
+	`)
+
+	assert_query_result(`
+		SELECT ?s ?p ?o ?pp ?oo ?ppp ?ooo
+		WHERE {
+			<<<<?s ?p ?o>> ?pp ?oo>> ?ppp ?ooo .
+		}
+		`,
+		`s   | p   | o   | pp  | oo  | ppp | ooo
+		 ============================================
+		 x:a | x:b | x:c | x:d | x:e | x:f | x:g
+		`)
 }
